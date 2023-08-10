9413da381eb4e55b4930f2f9029d3db854d06fba
unknown <g.anas786@gmail.com>
2023-08-10 23:04:37 +0530

    added bugfender sdk

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
+import { Bugfender } from '@bugfender/sdk';
 import Section from './components/Section';
 
 const App = () => {
+  useEffect(() => {
+    Bugfender.log('App Created');
+  }, []);
   return (
     <div className='bg-[#185459] h-screen'>
       <h1 className='text-center text-3xl font-bold text-[#fff] pt-[24px]'>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Bugfender } from '@bugfender/sdk';
+
+Bugfender.init({
+  appKey: 'lgLdGb0pz9JCphaegcXWSaQwf6QKvS00',
+  // apiURL: 'https://api.bugfender.com',
+  // baseURL: 'https://dashboard.bugfender.com',
+  // overrideConsoleMethods: true,
+  // printToConsole: true,
+  // registerErrorHandler: true,
+  // logBrowserEvents: true,
+  // logUIEvents: true,
+  // version: '',
+  // build: '',
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
